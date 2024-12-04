72183514552b3b0a2711e3716587eb811b5e1014
ghuali <guaghuali@gmail.com>
2024-12-04 11:39:46 +0000

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    println("Hola Mundo")
+    var entero:Int = 1
+    var str:String = "Mutumbu"
+    var boolean : Boolean = true
+    var double: Double = 2.0
+    var float: Float = 2.1F
+    println("Entero: " + entero + "\n" + "String: " + str + "\n" + "Boolean: " + boolean + "\n" + "Double: " + double + "\n" + "Float: " + float)
+    bucleFor()
+}
+
+//Bucle while
+fun bucleWhile() {
+    val animales = listOf("perro", "gato", "oso")
+    while ()
+}
+
+// Bucle for
+fun bucleFor() {
+    val frutas = listOf("apple", "banana", "kiwi")
+    for (fruta in frutas) {
+        println(fruta)
+    }
+}
